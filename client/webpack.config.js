cdc74749c0f111c93e6ae08f344a2f52008e9f38
asm2212 <asmareadmasu0@gmail.com>
2025-07-22 02:58:32 +0300

    fix:env error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
+
+const plugins = [
+  new HtmlWebpackPlugin({ template: 'public/index.html' }),
+];
+
+if (process.env.NODE_ENV !== 'production') {
+  const Dotenv = require('dotenv-webpack');
+  plugins.push(new Dotenv());
+}
+
+plugins.push(
+  new webpack.DefinePlugin({
+    'process.env.REACT_APP_PORT': JSON.stringify(process.env.REACT_APP_PORT || ''),
+    'process.env.REACT_APP_API_BASE_URL': JSON.stringify(process.env.REACT_APP_API_BASE_URL || ''),
+  })
+);
 
 module.exports = {
   entry: './src/index.jsx',
@@ -8,7 +24,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     clean: true,
-    publicPath: '/',  // <--- critical for Vercel
+    publicPath: '/',
   },
   resolve: { extensions: ['.js', '.jsx'] },
   module: {
@@ -18,10 +34,7 @@ module.exports = {
       { test: /\.svg$/, use: '@svgr/webpack' },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'public/index.html' }),
-    new Dotenv(),
-  ],
+  plugins,
   devServer: {
     port: 3001,
     historyApiFallback: true,
